32f4da9d44ae5c87d4718e73c64bad61f1961a10
Matheus Damião <matheus.damiaoliveira@gmail.com>
2024-11-06 10:26:34 -0300

    update: add correct domain to site url metadata

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,7 +5,7 @@ const config: GatsbyConfig = {
     title: `Del Bianco Domingos`,
     description: "Sociedade de Advocacia",
     image: `/icon.png`,
-    siteUrl: `https://del-bianco-santos-advocacia.netlify.app/`
+    siteUrl: `https://www.delbiancosantos.com.br/`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -17,11 +17,11 @@ const config: GatsbyConfig = {
       "icon": "src/images/icon.png"
     }
   },
-  'gatsby-plugin-postcss',
-  `gatsby-plugin-image`,
-  `gatsby-plugin-sharp`,
+    'gatsby-plugin-postcss',
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
 
-]
+  ]
 };
 
 export default config;
